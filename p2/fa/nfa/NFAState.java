e8c07cacfb72c91b594202582f0e677f8451960c
Kyle Epperson <kylestricken13@yahoo.com>
2020-10-25 16:39:10 -0600

    Added prelim structure of NFAState

--- p2/fa/nfa/NFAState.java
+++ p2/fa/nfa/NFAState.java
@@ -4,4 +4,71 @@ import fa.State;
 
 public class NFAState extends State {
     
+
+    private HashMap<Character,NFAState> delta;//delta
+	private boolean isFinal;//remembers its type
+    private NFAState previousState;
+    private int level;
+	
+	/**
+	 * Default constructor
+	 * @param name the state name
+	 */
+	public NFAState(String name){
+		initDefault(name);
+		isFinal = false;
+	}
+	
+	/**
+	 * Overlaoded constructor that sets the state type
+	 * @param name the state name
+	 * @param isFinal the type of state: true - final, false - nonfinal.
+	 */
+	public NFAState(String name, boolean isFinal){
+		initDefault(name);
+		this.isFinal = isFinal;
+	}
+	
+	private void initDefault(String name ){
+		this.name = name;
+		delta = new HashMap<Character, NFAState>();
+	}
+	
+	/**
+	 * Accessor for the state type
+	 * @return true if final and false otherwise
+	 */
+	public boolean isFinal(){
+		return isFinal;
+	}
+	
+
+	/**
+	 * Add the transition from <code> this </code> object
+	 * @param onSymb the alphabet symbol
+	 * @param toState to DFA state
+	 */
+	public void addTransition(char onSymb, NFAState toState){
+		delta.put(onSymb, toState);
+	}
+	
+	/**
+	 * Retrieves the state that <code>this</code> transitions to
+	 * on the given symbol
+	 * @param symb - the alphabet symbol
+	 * @return the new state 
+	 */
+	public NFAState getTo(char symb){
+		NFAState ret = delta.get(symb);
+		if(ret == null){
+			 System.err.println("ERROR: NFAState.getTo(char symb) returns null on " + symb + " from " + name);
+			 System.exit(2);
+			}
+		return delta.get(symb);
+	}
+	
+    public NFAState setPreviousState(NFAState previousState){
+        this.previousState = previousState;
+    }
+
 }
